7af589ffecec502fda1efa3f368eefbd54815777
Zeyad <j04w2w@inf.elte.hu>
2025-06-29 16:04:46 +0200

    average and positive feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,11 +3,11 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 
 const App = () => {
   // save clicks of each button to its own state
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  })
+  // const [clicks, setClicks] = useState({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0
+  // })
   const [good,setGood] = useState(0);
   const [neutral,setNeutral] = useState(0);
   const [bad,setBad] = useState(0);
@@ -24,6 +24,16 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      {good + neutral + bad  === 0 ?(
+        <p>No feedback given</p>
+      ) : (
+        <>
+        <p>average {(good - bad) / (good + neutral + bad)}</p>
+        <p>positive {good/(good + neutral + bad) * 100} %</p>
+        </>
+      )}
+      
       {/* <p>good {clicks.good}</p>
       <p>neutral {clicks.neutral}</p>
       <p>bad {clicks.bad}</p> */}
